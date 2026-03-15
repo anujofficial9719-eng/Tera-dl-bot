3a7478e5e7fdb5e42fca277ecaee06216b11a89d
anujofficial9719-eng <anujofficial9719@gmail.com>
2026-03-15 08:51:25 +0530

    Update downloader.py

--- downloader.py
+++ downloader.py
@@ -1,5 +1,4 @@
 import yt_dlp
-import re
 
 def normalize_link(url):
 
@@ -22,24 +21,18 @@ async def download_video(url, msg):
 
     url = normalize_link(url)
 
-    await msg.edit("Extracting video link...")
+    await msg.edit("🔍 Extracting video link...")
 
     ydl_opts = {
-        "format": "bestvideo+bestaudio/best",
+        "format": "best",
         "merge_output_format": "mp4",
-        "outtmpl": "video.%(ext)s",
+        "outtmpl": "video.mp4",
         "quiet": True
     }
 
     with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+        ydl.download([url])
 
-        info = ydl.extract_info(url, download=True)
+    await msg.edit("✅ Download complete")
 
-        filename = ydl.prepare_filename(info)
-
-        if not filename.endswith(".mp4"):
-            filename = filename.rsplit(".",1)[0] + ".mp4"
-
-    await msg.edit("Download complete")
-
-    return filename
+    return "video.mp4"
